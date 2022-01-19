9efd341b22f839196fe513d4db4a1147b2ca8a62
Candra <ecankurnia@gmail.com>
2022-01-19 20:49:31 +0700

    add logic LoginActivity

--- app/src/main/java/com/project/myambulance/LoginActivity.java
+++ app/src/main/java/com/project/myambulance/LoginActivity.java
@@ -8,29 +8,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.project.myambulance.databinding.ActivityLoginBinding;
+
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
 
-    Button login;
-    TextView reg;
+    ActivityLoginBinding activityLoginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = activityLoginBinding.getRoot();
+        setContentView(view);
+        onInit();
 
-        reg = findViewById(R.id.tv_regist);
-        login = findViewById(R.id.btn_login);
 
-        reg.setOnClickListener(view -> {
-            Intent regist = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(regist);
-            finish();
-        });
-        login.setOnClickListener(view -> {
+        activityLoginBinding.tvRegist.setOnClickListener(view1 -> {
             Intent login = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(login);
             finish();
         });
 
+    }
+
+    private void onInit() {
+        activityLoginBinding.btnLogin.setOnClickListener(view -> {
+            validation();
+        });
+    }
+
+    private void validation() {
+        String username = Objects.requireNonNull(activityLoginBinding.edtUsername.getText()).toString();
+        String password = Objects.requireNonNull(activityLoginBinding.edtPassword.getText()).toString();
     }
 }
--- app/src/main/java/com/project/myambulance/remote/RemoteEndpoint.java
+++ app/src/main/java/com/project/myambulance/remote/RemoteEndpoint.java
@@ -3,5 +3,8 @@ package com.project.myambulance.remote;
 public class RemoteEndpoint {
     public static final String ENDPOINT_GET_HISTORY = "crud1.php?function=get_history";
     public static final String ENDPOINT_GET_COVID = "http://apicovid19indonesia-v2.vercel.app/api/indonesia";
+    public static final String ENDPOINT_REGISTER = "dbambulance/registeruser.php";
+    public static final String ENDPOINT_LOGIN = "dbambulance/login.php";
+
 
 }
